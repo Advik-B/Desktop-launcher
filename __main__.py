66aab3ff866d4ce17a04e716b47158ab7ef2a6c9
Advik <advik.b@gmail.com>
2022-09-11 20:20:12 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,35 +1,37 @@
-from PyQt6.QtWidgets import (
-    QApplication,
-    QWidget,
-    QGridLayout,
-    QLabel,
-    QLineEdit,
-    QPushButton,
-    QMainWindow,
-    QMessageBox,
-)
+import json
+import os
+import sys
+from ctypes import windll
+from os.path import join as pjoin
 
 # from PyQt5.QtGui import QPixmap
 from PIL.ImageQt import QPixmap
-from PyQt6.QtGui import QFont, QFontDatabase, QAction, QShortcut, QKeySequence
 from PyQt6.QtCore import Qt
-import sys
-import os
-from os.path import join as pjoin
-from ctypes import windll
+from PyQt6.QtGui import QAction, QFont, QFontDatabase, QKeySequence, QShortcut
+from PyQt6.QtWidgets import (
+    QApplication,
+    QGridLayout,
+    QLabel,
+    QLineEdit,
+    QMainWindow,
+    QMessageBox,
+    QPushButton,
+    QWidget,
+)
 
 AppID = "advik.desktop_launcher.1"
 windll.shell32.SetCurrentProcessExplicitAppUserModelID(AppID)
 
 from plyer import notification
 
-
 notification.notify(
-    title = 'testing',
-    message = 'message',
-    app_icon = "assets/rocket.ico",
-    timeout = 10,
+    title="testing",
+    message="message",
+    app_icon="assets/rocket.ico",
+    timeout=10,
 )
+
+
 class UI(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -48,9 +50,9 @@ class UI(QMainWindow):
         self.setWindowFlags(
             # winT.WindowOverridesSystemGestures |
             # winT.NoDropShadowWindowHint |
-            winT.WindowTitleHint |
-            winT.CustomizeWindowHint |
-            winT.WindowCloseButtonHint #|
+            winT.WindowTitleHint
+            | winT.CustomizeWindowHint
+            | winT.WindowCloseButtonHint  # |
             # winT.WindowMaximizeButtonHint
         )
         del winT
@@ -64,7 +66,9 @@ class UI(QMainWindow):
     def resizeEvent(self, a0):
         super().resizeEvent(a0)
         self.backgroundimage.resize(self.size())
-        self.backgroundimage_pixmap = self.backgroundimage_pixmap_orignal.scaled(self.size())
+        self.backgroundimage_pixmap = self.backgroundimage_pixmap_orignal.scaled(
+            self.size()
+        )
         self.backgroundimage.setPixmap(self.backgroundimage_pixmap)
 
     def closeEvent(self, a0):
@@ -86,21 +90,28 @@ class UI(QMainWindow):
         else:
             self.showMaximized()
             self.maxi = True
+
+
 def main():
     global QFontDatabase
     app = QApplication(sys.argv)
     fontDB = QFontDatabase
     del QFontDatabase
 
-    for root, dirs, files in os.walk('fonts'):
+    for root, dirs, files in os.walk("fonts"):
         for file in files:
-            if file.endswith('.ttf') or file.endswith('.otf'):
+            if file.endswith(".ttf") or file.endswith(".otf"):
                 pth = pjoin(root, file).replace("\\", "/")
                 fontDB.addApplicationFont(pth)
                 del pth
     del root, dirs, files, file
     ui = UI()
     app.exec()
+
+
+def create_settings():
+    pass
+
 
 if __name__ == "__main__":
     main()
